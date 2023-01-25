8b0b8d1603ae6242013376df8421c4340a1d02e8
deimarut <deimante.rutenaite@gmail.com>
2023-01-25 18:54:59 +0200

    setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router'
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/attendees" element={<div>Attendees</div>} />
+      </Routes>
+    </div>
+  )
+  }
+export default App;
