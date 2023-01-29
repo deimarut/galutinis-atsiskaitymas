bda1cbf90e4fedb86ab8d31a3684a155f864d2d4
deimarut <deimante.rutenaite@gmail.com>
2023-01-29 22:22:49 +0200

    pakeitimai FE ir BE

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
 
 require('dotenv').config();
 
@@ -24,6 +25,60 @@ app.get('/attendees', (req, res) => {
     connection.execute('SELECT * FROM attendees WHERE userId=?', [userId], (err, attendees) => {
         res.send(attendees);
     });
+});
+
+app.post('/attendees', (req, res) => {
+    const {name, surname, email, phone, userId} = req.body;
+
+    connection.execute(
+        'INSERT INTO attendees (name, surname, email, phone, userId) VALUES (?, ?, ?, ?, ?)', 
+        [name, surname, email, phone, userId],
+        () => {
+            connection.execute(
+                'SELECT * FROM attendees WHERE userId=?', 
+                [userId], 
+                (err, attendees) => {
+                    res.send(attendees);
+                }
+            )
+        }
+    )
+});
+
+app.post('/register', (req, res) => {
+    const { email, name, surname, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 12);
+    bcrypt.compareSync();
+
+    connection.execute(
+        'INSERT INTO users (email, name, surname, password) VALUES (?, ?, ?, ?)', 
+        [email, name, surname, hashedPassword], 
+        (err, result) => {
+            res.sendStatus(200);
+        }
+    )
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    connection.execute(
+        'SELECT * FROM users WHERE email=?', 
+        [email], 
+        (err, result) => {
+            if (result.length === 0) {
+                res.send('Incorrect username or password');
+            } else {
+                const passwordHash = result[0].password;
+                const isPasswordCorrect = bcrypt.compareSync(password, passwordHash);
+                if (isPasswordCorrect) {
+                    res.send('Successfully logged in');
+                } else {
+                    res.send('Incorrect username or password');
+                }
+            }
+        }
+    );
 });
 
 const PORT = 8080;
--- client/src/pages/Attendees/Attendees.jsx
+++ client/src/pages/Attendees/Attendees.jsx
@@ -6,6 +6,10 @@ import { AttendeesID, AttendeesList, AttendeesListItem } from "./style";
 export const Attendees = () => {
     const [attendees, setAttendees] = useState([]);
     const [isLoading, setIsLoading] = useState([true]);
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [email, setEmail] = useState(''); 
+    const [phone, setPhone] = useState('');
 
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_URL}/attendees?userId=${LOGGED_IN_USER.id}`)
@@ -20,8 +24,62 @@ export const Attendees = () => {
         return <div>Loading...</div>;
     }
 
+    const handleAttendeesAdd = (e) => {
+        e.preventDefault();
+        fetch(`${process.env.REACT_APP_API_URL}/attendees`, {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name, 
+                surname, 
+                email, 
+                phone, 
+                userId: 1
+            })
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            setAttendees(data);
+            setName('');
+            setSurname('');
+            setEmail('');
+            setPhone('');
+        });
+    }
+
     return (
         <AttendeesList>
+            <form onSubmit={handleAttendeesAdd}>
+                <input 
+                    placeholder="Vardas" 
+                    required 
+                    onChange={(e) => setName(e.target.value)} 
+                    value={name}
+                />
+                <input 
+                    placeholder="Pavardė" 
+                    required 
+                    onChange={(e) => setSurname(e.target.value)} 
+                    value={surname}
+                />
+                <input 
+                    placeholder="Elektroninis paštas" 
+                    type="email" 
+                    required 
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+                <input 
+                    placeholder="Telefono numeris 370..." 
+                    required 
+                    onChange={(e) => setPhone(e.target.value)}
+                    value={phone}
+                />
+                <button>Pridėti į svečių sąrašą</button>
+            </form>
+
             {attendees.map((att) => (
                 <AttendeesListItem key={att.id}>
                     <AttendeesID>Eilės numeris: {att.id}</AttendeesID>
