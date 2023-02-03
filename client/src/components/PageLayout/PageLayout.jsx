6dbb73a9a2d02d053a7fe74c34786199411bfe32
deimarut <deimante.rutenaite@gmail.com>
2023-02-04 00:31:07 +0200

    mygtukas atsijungti + headeris

--- client/src/components/PageLayout/PageLayout.jsx
+++ client/src/components/PageLayout/PageLayout.jsx
@@ -1,16 +1,30 @@
 import { useContext } from "react";
-import { Navigate, Outlet } from "react-router";
+import { Navigate, Outlet, useNavigate } from "react-router";
 import { UserContext } from "../../contexts/UserContextWrapper";
+import { Button } from '../Button/Button';
+import { Header } from "./PageLayoutStyle";
+import {LOCAL_STORAGE_JWT_TOKEN_KEY} from '../../constants/constants';
 
 export const PageLayout = () => {
-    const { user } = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
+    const navigate = useNavigate();
 
     if (!user) {
         return <Navigate to="/login" />
     }
 
+    const handleLogout = () => {
+        localStorage.removeItem(LOCAL_STORAGE_JWT_TOKEN_KEY)
+        setUser(null);
+        navigate('/login');
+    }
+
     return (
         <div>
+            <Header>
+                <h1>Renginio dalyvių registravimo puslapis</h1>
+                <Button onClick={handleLogout}>Atsijungti</Button>
+            </Header>
             <Outlet />
         </div>
     )
